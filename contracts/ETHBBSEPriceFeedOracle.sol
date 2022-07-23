8f0be01b7212ae82ed570b5161256838444cfded
ge86soz <ge68soz@um.de>
2022-07-23 17:22:15 +0200

    Updated the solutions

--- contracts/ETHBBSEPriceFeedOracle.sol
+++ contracts/ETHBBSEPriceFeedOracle.sol
@@ -4,40 +4,49 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ETHBBSEPriceFeedOracle is Ownable {
-  // Max number of blocks before a price update is required
-  uint8 public constant MAX_PRICE_AGE = 3;
+    // Max number of blocks before a price update is required
+    uint8 public constant MAX_PRICE_AGE = 3;
 
-  // ETH/BBSE rate
-  uint private rate;
+    // ETH/BBSE rate
+    uint private rate;
 
-  // Block number of the last rate update.
-  uint public lastUpdateBlock;
+    // Block number of the last rate update.
+    uint public lastUpdateBlock;
 
-  // An event that indicates the price of the priceFeed should be updated
-  // Must be listened by the oracle server
-  event GetNewRate (string priceFeed);
-  
-  /**
-  * @dev Initializes lastUpdateBlock to current block number and rate to 0.
+    // An event that indicates the price of the priceFeed should be updated
+    // Must be listened by the oracle server
+    event GetNewRate (string priceFeed);
+
+    /**
+    * @dev Initializes lastUpdateBlock to current block number and rate to 0.
   * Emits GetNewRate to trigger the oracle server to update the rate.
   * The priceFeed parameter of GetNewRate should be ETH/BBSE
   */
-  constructor () public { 
-    // TODO: Complete the constructor
-  }
+    constructor () public {
+        lastUpdateBlock = block.number;
+        rate = 0;
+        emit GetNewRate("ETH/BBSE");
+    }
 
-  /**
-  * @dev Updates the rate and sets the lastUpdateBlock to current block number.
+    /**
+    * @dev Updates the rate and sets the lastUpdateBlock to current block number.
   * Can only be called by the owner of the oracle contract.
   * Can't be called internally.
   * @param _rate new rate of the price feed.
   */
-  // TODO: Implement the updateRate function (use the respective function modifier from Ownable)
-
-  /**
-  * @dev Returns the current rate.
+    function updateRate(uint _rate) external onlyOwner {
+        rate = _rate;
+        lastUpdateBlock = block.number;
+    }
+    /**
+    * @dev Returns the current rate.
   * If rate was updated more than MAX_PRICE_AGE block ago,
   * emits GetNewRate event to trigger the oracle server.
   */
-  // TODO: Implement the getRate function
+    function getRate() public returns (uint){
+        if (block.number - lastUpdateBlock > MAX_PRICE_AGE) {
+            emit GetNewRate("ETH/BBSE");
+        }
+        return rate;
+    }
 }
